157b8e8808ac36c5db32d6319de45fb7a4a9b20e
Saman Ghorban <saman@finterra.local>
2024-06-05 11:31:05 +0800

    second commit

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
-// const express = require('express');
-// const app = express();
-
-const { test } = require("./src/repository/profile.repository");
-
-// const authRouter = require('./src/api/auth/auth.router/index');
-// const homeRouter = require('./src/api/home/home.router/index');
-// const profileRouter = require('./src/api/profile/profile.router/index');
-
-// app.use(express.json())
-// app.use(express.urlencoded({extended: false}))
+const express = require('express');
+const app = express();
 
 
-// app.use('/', authRouter)
-// app.use('/', homeRouter)
-// app.use('/', profileRouter)
+const authRouter = require('./src/api/auth/auth.router/index');
+const homeRouter = require('./src/api/home/home.router/index');
+const profileRouter = require('./src/api/profile/profile.router/index');
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
+app.use('/', authRouter)
+app.use('/', homeRouter)
+app.use('/', profileRouter)
 
 
 
-// app.listen(5000, ()=> {
-//     console.log("App runing on port 5000");
-// })
-
-
-test('saman@gmail.com').then(res => {
-    console.log(res);
+app.listen(5000, ()=> {
+    console.log("App runing on port 5000");
 })
+
